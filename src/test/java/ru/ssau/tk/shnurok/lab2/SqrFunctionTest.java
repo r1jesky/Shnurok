5977f61c47759dbed47e39f4a68fb6d3ebec2a63
r1jesky <gggggbgfrhxt@gmail.com>
2024-10-02 12:01:54 +0400

    test ready

--- src/test/java/ru/ssau/tk/shnurok/lab2/SqrFunctionTest.java
+++ src/test/java/ru/ssau/tk/shnurok/lab2/SqrFunctionTest.java
@@ -5,4 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SqrFunctionTest {
 
+    @Test
+    public void testApply_ReturnsValidResult(){
+        SqrFunction sqrFunction = new SqrFunction();
+
+        assertEquals(1,sqrFunction.apply(1));
+        assertEquals(1,sqrFunction.apply(-1));
+        assertEquals(4,sqrFunction.apply(2));
+        assertEquals(0,sqrFunction.apply(0));
+        assertEquals(1.44,sqrFunction.apply(1.2));
+    }
 }
