ef08fe8860dd8ed1a90c4254b5dfdbc38b40290b
r1jesky <gggggbgfrhxt@gmail.com>
2024-10-08 22:24:06 +0400

    MathFunction edit

--- src/main/java/ru/ssau/tk/shnurok/lab2/coredefenitions/MathFunction.java
+++ src/main/java/ru/ssau/tk/shnurok/lab2/coredefenitions/MathFunction.java
@@ -6,7 +6,7 @@ public interface MathFunction {
     double apply(double x);
 
     default CompositeFunction andThen(MathFunction  afterFunction){
-        return new CompositeFunction(this, afterFunction);
+        return  new CompositeFunction(this, afterFunction);
     }
     
 }
