d02794f017eec01f8fac74a33d0d1e83c62344b7
r1jesky <gggggbgfrhxt@gmail.com>
2024-10-18 12:58:06 +0400

    Point add

---
+++ src/main/java/ru/ssau/tk/shnurok/lab2/realizations/Point.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.shnurok.lab2.realizations;
+
+public class Point {
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
