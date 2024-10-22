7d14acbf8a7fef55baa3d367c0ac9a628d76f29d
r1jesky <gggggbgfrhxt@gmail.com>
2024-10-23 00:08:50 +0400

    AsPoints add

--- src/main/java/ru/ssau/tk/shnurok/lab2/functions/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/shnurok/lab2/functions/operations/TabulatedFunctionOperationService.java
@@ -4,14 +4,14 @@ import ru.ssau.tk.shnurok.lab2.functions.coredefenitions.TabulatedFunction;
 import ru.ssau.tk.shnurok.lab2.functions.realizations.Point;
 
 public class TabulatedFunctionOperationService {
-    static Point[] asPoints(TabulatedFunction tabulatedFunction){
+    public static Point[] asPoints(TabulatedFunction tabulatedFunction){
         int count = tabulatedFunction.getCount(); // Получаем количество точек
         Point[] points = new Point[count]; // Инициализируем массив точек
 
         int i = 0;
         for (Point point : tabulatedFunction) { // Проходим по табулированной функции
-            points[i] = point; // Записываем точку в массив
-            i++;
+            points[i++] = point; // Записываем точку в массив
+
         }
         return points; // Возвращаем массив точек
     }
