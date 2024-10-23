5187d35c7639e02ff9978227978d964a60b43048
r1jesky <gggggbgfrhxt@gmail.com>
2024-10-23 23:34:33 +0400

    exceptionsTests

---
+++ src/test/java/ru/ssau/tk/shnurok/lab2/functions/exeptions/DifferentLengthOfArraysExceptionTest.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.shnurok.lab2.functions.exeptions;
+
+import org.junit.jupiter.api.Test;
+import ru.ssau.tk.shnurok.lab2.exeptions.DifferentLengthOfArraysException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DifferentLengthOfArraysExceptionTest {
+
+    @Test
+    void testDefaultConstructor() {
+        DifferentLengthOfArraysException exception = new DifferentLengthOfArraysException();
+        assertNull(exception.getMessage(), "Сообщение исключения должно быть null");
+    }
+
+    @Test
+    void testConstructorWithMessage() {
+        String message = "Длины массивов различны";
+        DifferentLengthOfArraysException exception = new DifferentLengthOfArraysException(message);
+        assertEquals(message, exception.getMessage(), "Сообщение исключения не совпадает с ожидаемым");
+    }
+}
