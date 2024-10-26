1af1606e28751ef1386e637247d06dff2f69ee41
r1jesky <gggggbgfrhxt@gmail.com>
2024-10-26 17:28:18 +0400

    SynchronizedTabulatedFunction add

---
+++ src/main/java/ru/ssau/tk/shnurok/lab2/concurrent/SynchronizedTabulatedFunction.java
@@ -0,0 +1,64 @@
+package ru.ssau.tk.shnurok.lab2.concurrent;
+
+import ru.ssau.tk.shnurok.lab2.functions.coredefenitions.TabulatedFunction;
+import ru.ssau.tk.shnurok.lab2.functions.realizations.Point;
+
+import java.util.Iterator;
+
+public class SynchronizedTabulatedFunction implements TabulatedFunction {
+    private final TabulatedFunction function;
+
+    public SynchronizedTabulatedFunction(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public int getCount() {
+        return function.getCount();
+    }
+
+    @Override
+    public double getX(int index) {
+        return function.getX(index);
+    }
+
+    @Override
+    public double getY(int index) {
+        return function.getY(index);
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        function.setY(index,value);
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        return function.indexOfX(x);
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        return function.indexOfY(y);
+    }
+
+    @Override
+    public double leftBound() {
+        return function.leftBound();
+    }
+
+    @Override
+    public double rightBound() {
+        return function.rightBound();
+    }
+
+    @Override
+    public Iterator<Point> iterator() {
+        return null;
+    }
+
+    @Override
+    public double apply(double x) {
+        return function.apply(x);
+    }
+}
