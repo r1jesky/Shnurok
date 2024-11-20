a03a73dae18be87bc83330a7bcc52dd87f393f5a
Perfileva Darya <darriora@gmail.com>
2024-11-20 22:45:58 +0400

    add security

---
+++ src/main/java/ru/ssau/tk/shnurok/lab2/security/JwtAuthTokenFilter.java
@@ -0,0 +1,55 @@
+package ru.ssau.tk.shnurok.lab2.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import ru.ssau.tk.shnurok.lab2.model.UserDetailsServiceImpl;
+
+import java.io.IOException;
+
+@Component
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class JwtAuthTokenFilter extends OncePerRequestFilter {
+
+    private JwtUtils jwtUtils;
+
+    private UserDetailsServiceImpl userDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String username = jwtUtils.getUsernameFromJwtToken(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+            filterChain.doFilter(request, response);
+    }
+
+
+    private String parseJwt (HttpServletRequest request){
+        String headerAuth = request.getHeader("Authorization");
+        if (headerAuth != null && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/ru/ssau/tk/shnurok/lab2/security/JwtResponse.java
@@ -0,0 +1,20 @@
+package ru.ssau.tk.shnurok.lab2.security;
+
+public class JwtResponse {
+
+    private String token;
+
+    public JwtResponse() {}
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
